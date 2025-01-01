5154b92b2c921104f2b42b82102575672431b548
lrpalmer27 <lrpalmer@mun.ca>
2025-01-01 17:53:24 -0600

    Timelapse running

---
+++ TimelapseMaker.py
@@ -0,0 +1,37 @@
+"""
+This file is intended to make a timelapse for another project. 
+I want to make a timelapse of this: https://github.com/lrpalmer27/MAPLIGHTS
+
+"""
+
+import io
+import os
+import time
+import datetime
+from datetime import timedelta
+from picamera import PiCamera
+
+# ----------------------- PREAMBLE ---------------------------------
+SaveFolder='TimelapsePictures'
+
+RESOLUTION = (1280,720) #1080p (camera max)
+NumPics=100 #number of pics that will be saved over period of time
+PeriodOfTime=18 #number of hours to record over
+TimeDelay=int((60*60*PeriodOfTime)/NumPics) #time delay in seconds between pictures.
+
+# ----------------------- INIT CAMERA ------------------------------
+camera=PiCamera()
+camera.resolution=RESOLUTION
+camera.rotation=180 #camera is mounted upside down on rpi3b mount.
+
+# ----------------------- START PICTURE TAKING ---------------------
+StartingTime=datetime.datetime.now()
+CurrentTime=datetime.datetime.now()
+
+while StartingTime < CurrentTime:
+    CurrentTime=(datetime.datetime.now()).replace(microsecond=0)
+    CurrentTime=str(CurrentTime).replace(':',"-")
+    CurrentTime=CurrentTime.replace(" ","_")
+    camera.capture(rf"{SaveFolder}/{CurrentTime}.png")
+    print(f'Picture taken at {CurrentTime} & saved.')
+    time.sleep(TimeDelay)
